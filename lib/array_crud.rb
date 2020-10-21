9189418d683308e02206c8dd4a08dee4ff9e20d6
Jacob Tobias <jtobias811@gmail.com>
2020-10-21 16:05:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  arrays! = [].pop("arrays!")
+  [].pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
